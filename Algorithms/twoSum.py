bee583c4c51d73e542731ef19c6b44b38f94d1c3
NaughtyFlame <andrewattention@gmail.com>
2017-04-03 20:29:45 +0200

    twoSum

---
+++ Algorithms/twoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return False
+        hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash:
+                return [hash[target - nums[i]], i]
+            hash[nums[i]] = i
+        #find nothing
+        return [-1, -1]
