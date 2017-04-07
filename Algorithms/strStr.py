abb21b0f59f2eac2769be465264390ba916d11f8
NaughtyFlame <andrewattention@gmail.com>
2017-04-07 18:15:53 +0200

    strStr

---
+++ Algorithms/strStr.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack is None or needle is None:
+        	return -1
+        for i in range(0,len(haystack) - len(needle) + 1):
+        	j = 0
+        	while (j < len(needle)):
+        	 	if haystack[i+j] != needle[j]:
+        	 		break
+        	 	j += 1
+        	if j == len(needle):
+        	 	return i
+        return -1
