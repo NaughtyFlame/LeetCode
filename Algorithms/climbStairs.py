4cdc852fd610e10b35803167d03ffff73912d7b6
NaughtyFlame <andrewattention@gmail.com>
2017-05-05 19:52:58 +0200

    ClimbStairs

---
+++ Algorithms/climbStairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def climbStairs(self, n):
+        # write your code here
+        if n <= 2:
+            return n
+        result=[1,2]
+        for i in range(n-2):
+            result.append(result[-2]+result[-1])
+        return result[-1]
