8f6385e6292819cfd4c14e7515a79fc7214a405b
SHI Mengjie <andrewattention@gmail.com>
2017-09-13 15:02:12 +0200

    Create Valid_Palindrome.py

---
+++ Algorithms/Valid_Palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start, end = 0, len(s) - 1
+        while start < end:
+            #remove space and special char
+            while start < end and not s[start].isalpha() and not s[start].isdigit():
+                start += 1
+            while start < end and not s[end].isalpha() and not s[end].isdigit():
+                end -= 1
+            #campare
+            if start < end and s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -=1
+        return True    
