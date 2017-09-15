f37a9315e687745ec001d52df69e9509ed324853
SHI Mengjie <andrewattention@gmail.com>
2017-09-15 17:00:17 +0200

    Create 189_Rotate_Array.py

---
+++ Algorithms/189_Rotate_Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for index in range(k):
+            x = nums.pop()
+            nums.insert(0,x)
