8be44b3e57d081aa6c8ad4f1f4146214c0339c7e
SHI Mengjie <andrewattention@gmail.com>
2017-09-18 11:51:25 +0200

    Create 206_Reverse_Linked_List.py

---
+++ Algorithms/200-299/206_Reverse_Linked_List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+        p = head.next
+        n = self.reverseList(p)
+        head.next = None
+        p.next = head
+        return n
