f7ed2b9f11d3bbcdc70ca2be541e7f478ea3adb9
SHI Mengjie <andrewattention@gmail.com>
2017-09-20 15:54:36 +0200

    Create 268_Missing_Number.py

---
+++ Algorithms/200-299/268_Missing_Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
