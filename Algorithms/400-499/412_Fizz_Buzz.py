4ea05f5a908d3eba45cbc722516505452944fb8c
SHI Mengjie <andrewattention@gmail.com>
2017-09-27 11:34:46 +0200

    Create 412_Fizz_Buzz.py

---
+++ Algorithms/400-499/412_Fizz_Buzz.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append("Fizz")
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+                
+        return res
+"""
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+"""
